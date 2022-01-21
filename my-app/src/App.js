9f16b419e09fa2d77a2831a96250cf46dddbf09e
Nawapol <arbruzaz1@gmail.com>
2022-01-21 23:49:50 +0700

    add package

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import {Container} from "semantic-ui-react";
+import TodoList from "./Todolist"
+
+function App(){
+  return(
+    <div>
+      <Container>
+        {/* <TodoList/> */}
+        <div>Boat</div>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
